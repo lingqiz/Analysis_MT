6ec35204b3ff52a559917c37c8b1ce7150a897dd
lingqiz <zlqzcc@gmail.com>
2021-06-07 13:40:53 -0400

    add plot

--- thresholdPred.m
+++ thresholdPred.m
@@ -27,14 +27,15 @@ load('./MappingFit/new_para_map_fit/new_para_Feb9.mat');
 
 nSub = 5;
 allPara = [paraSub1; paraSub2; paraSub3; paraSub4; paraSub5];
+scale = 0.005;
 
 for i = [1, 2, 4, 5]
     para = allPara(i, :);
-    plotThreahold(para, true);
+    plotThreahold(para, scale, true);
 end
 
 load('CombinedFit/combinedMapping.mat');
-plotThreahold(paraSub, true);
+plotThreahold(paraSub, scale, true);
 
 labelPos = [0.25, 0.5, 1, 2.0, 4.0, 8.0, 16, 32];
 xticks(log(labelPos));
@@ -46,20 +47,33 @@ grid off;
 xlabel('Speed');
 ylabel('Weber Fraction');
 
+% Data
+subplot(1, 2, 1); hold on;
+load('McKee_Data.csv');
+plot(log(McKee_Data(:, 1)), McKee_Data(:, 2), 'o');
+
+load('Bruyn_Data.csv');
+plot(log(Bruyn_Data(1:7, 1)), Bruyn_Data(1:7, 2), 's');
+
+% labelPos = [0.05, 0.1, 0.25, 0.5, 1, 2.0, 4.0, 8.0, 20, 40];
+% xticks((labelPos));
+% xticklabels(arrayfun(@num2str, labelPos, 'UniformOutput', false));
+
 %% Plot Threshold 2
 subplot(1, 2, 2); hold on;
 load('./MappingFit/new_para_map_fit/new_para_Feb9.mat');
 
 nSub = 5;
 allPara = [paraSub1; paraSub2; paraSub3; paraSub4; paraSub5];
+scale = 0.00475;
 
 for i = [1, 2, 4, 5]
     para = allPara(i, :);
-    plotThreahold(para, false);
+    plotThreahold(para, scale, false);
 end
 
 load('CombinedFit/combinedMapping.mat');
-plotThreahold(paraSub, false);
+plotThreahold(paraSub, scale, false);
 
 legend({'1', '2', '3', '4', 'Com'}, 'Location', 'northeast');
 
@@ -67,8 +81,54 @@ grid off;
 xlabel('Speed');
 ylabel('Weber Fraction');
 
+% Data
+subplot(1, 2, 2); hold on;
+load('McKee_Data.csv');
+plot((McKee_Data(:, 1)), McKee_Data(:, 2), 'o');
+
+load('Bruyn_Data.csv');
+plot((Bruyn_Data(1:7, 1)), Bruyn_Data(1:7, 2), 's');
+
+% labelPos = [0.05, 0.1, 0.25, 0.5, 1, 2.0, 4.0, 8.0, 20, 40];
+% xticks((labelPos));
+% xticklabels(arrayfun(@num2str, labelPos, 'UniformOutput', false));
+
+%% Final Plot
+plotlabOBJ = plotlab();
+plotlabOBJ.applyRecipe(...
+    'figureWidthInches', 9, ...
+    'figureHeightInches', 8);
+
+figure(); hold on;
+load('./MappingFit/new_para_map_fit/new_para_Feb9.mat');
+
+nSub = 5;
+allPara = [paraSub1; paraSub2; paraSub3; paraSub4; paraSub5];
+scale = 0.00425;
+
+for i = [1, 2, 4, 5]
+    para = allPara(i, :);
+    plotThreahold(para, scale, false);
+end
+
+load('CombinedFit/combinedMapping.mat');
+plotThreahold(paraSub, scale, false);
+
+legend({'1', '2', '3', '4', 'Com'}, 'Location', 'northeast');
+
+grid off;
+xlabel('Speed');
+ylabel('Weber Fraction');
+
+% Data
+load('McKee_Data.csv');
+plot((McKee_Data(:, 1)), McKee_Data(:, 2), 'o');
+
+load('Bruyn_Data.csv');
+plot((Bruyn_Data(1:7, 1)), Bruyn_Data(1:7, 2), 's');
+
 %% Helper function
-function plotThreahold(para, logSpace)
+function plotThreahold(para, scale, logSpace)
 c0 = para(1); c1 = para(2); c2 = para(3);
 domain    = -100 : 0.01 : 100;
 
@@ -76,9 +136,9 @@ priorUnm  = 1.0 ./ ((abs(domain) .^ c0) + c1) + c2;
 nrmConst  = 1.0 / (trapz(domain, priorUnm));
 prior = @(support) (1.0 ./ ((abs(support) .^ c0) + c1) + c2) * nrmConst;
 
-UB = 40; priorSupport = (0.25 : 0.001 : UB);
+UB = 40; priorSupport = (0.2 : 0.001 : UB);
 
-fraction = 1 ./ prior(priorSupport) ./ priorSupport * 0.025;
+fraction = 1 ./ prior(priorSupport) ./ priorSupport * scale;
 
 if logSpace
     plot(log(priorSupport), fraction);
